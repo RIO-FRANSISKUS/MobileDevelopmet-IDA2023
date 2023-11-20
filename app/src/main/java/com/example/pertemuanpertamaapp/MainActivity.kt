6a9c72ea3729b5af7de44ccca5e9992a65115b2f
RioFransiskus <richardjuntaktombuk16@gmail.com>
2023-11-20 21:14:04 +0700

    Pertemuan 3 (Intent)

--- app/src/main/java/com/example/pertemuanpertamaapp/MainActivity.kt
+++ app/src/main/java/com/example/pertemuanpertamaapp/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.pertemuanpertamaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnExplicitIntent : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnExplicitIntent = findViewById(R.id.btn_explicit_intent)
+
+        btnExplicitIntent.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_explicit_intent -> {
+                val moveActivity = Intent(this@MainActivity, ExplicitIntentActivity::class.java)
+                startActivity(moveActivity)
+            }
+
+
+        }
     }
 }

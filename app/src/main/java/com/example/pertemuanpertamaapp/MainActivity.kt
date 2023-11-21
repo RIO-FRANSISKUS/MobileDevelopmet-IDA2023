9428979aecfd7e7ea24b6f60b4e3275ca7ce2572
RioFransiskus <richardjuntaktombuk16@gmail.com>
2023-11-21 20:28:13 +0700

    Pertemuan 3 (Recycle View)

--- app/src/main/java/com/example/pertemuanpertamaapp/MainActivity.kt
+++ app/src/main/java/com/example/pertemuanpertamaapp/MainActivity.kt
@@ -4,12 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvHero: RecyclerView
+    private val list = ArrayList<Hero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        rvHero = findViewById(R.id.rv_hero)
+        rvHero.setHasFixedSize(true)
+
+        list.addAll(getListHero())
+        showRecycleList()
+    }
+
+    private fun showRecycleList() {
+        rvHero.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHero.adapter = listHeroAdapter
+    }
+
+    private fun getListHero(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+
+        val listHero = ArrayList<Hero>()
+
+        for (i in dataName.indices){
+            val hero = Hero(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)
+        }
+        return listHero
     }
 }
